5dff9dcf8d61cee703a4fd7a969fa6b0aa64f1be
youngzhenhao <2499342961@qq.com>
2024-07-11 10:45:49 +0800

    update 0002

--- 0002/main.go
+++ 0002/main.go
@@ -1,5 +1,42 @@
 package main
 
+// https://leetcode.cn/problems/add-two-numbers/description/
 func main() {
 
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var tail, head *ListNode
+	carry := 0
+	for l1 != nil || l2 != nil {
+		sum := carry
+		v1, v2 := 0, 0
+		if l1 != nil {
+			v1 = l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			v2 = l2.Val
+			l2 = l2.Next
+		}
+		sum = v1 + v2 + carry
+		carry = sum / 10
+		sum %= 10
+		if head == nil {
+			head = &ListNode{sum, nil}
+			tail = head
+		} else {
+			tail.Next = &ListNode{sum, nil}
+			tail = tail.Next
+		}
+	}
+	if carry > 0 {
+		tail.Next = &ListNode{carry, nil}
+	}
+	return head
 }
